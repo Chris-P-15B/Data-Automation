93069cc89f590b74d6f3c140640f51b2f0cc54d9
Chris-P-15B <40764876+Chris-P-15B@users.noreply.github.com>
2020-01-06 13:46:58 +0000

    Add files via upload

--- connected_mac_addresses.py
+++ connected_mac_addresses.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         # Grab interface status
         cli_output = device.send_command("show interface status | include connected")
         if cli_output == None or len(cli_output) == 0:
-            print(target_switch + " has no interfaces.")
+            print(f"{target_switch} has no interfaces.")
             sys.exit(0)
         cli_output = cli_output.split("\n")
         interface_list = []
