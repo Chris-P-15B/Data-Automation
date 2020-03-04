109f3bb10ff54b623a05602738f5e3a21fa4e47f
Chris-P-15B <40764876+Chris-P-15B@users.noreply.github.com>
2020-03-04 09:53:51 +0000

    Add files via upload

--- mac_address_locator.py
+++ mac_address_locator.py
@@ -4,7 +4,7 @@
 
 # Returns list of interfaces on switches specified in CSV file that have learnt a given MAC address
 
-# v1.2 - added device auto-detection
+# v1.2 - added device auto-detection, added Arista support
 # v1.1 - fixed edge case for empty show output
 # v1.1 - code tidying
 # v1.0 - initial release
@@ -54,24 +54,24 @@ def validate_mac_address(mac_address):
     mac_address = mac_address[0:4] + "." + mac_address[4:8] + "." + mac_address[8:12]
     return mac_address
 
-def find_mac_address(switch, mac_address, results_list):
+def find_mac_address(remote_device, mac_address, results_list):
     """Updates list of interfaces that have learnt the specified MAC address"""
     try:
         # Auto-detect device type & establish correct SSH connection
-        best_match = guess_device_type(switch)
+        best_match = guess_device_type(remote_device)
         if best_match is None:
             return
         else:
-            switch["device_type"] = best_match
-        device = ConnectHandler(**switch)
+            remote_device["device_type"] = best_match
+        device = ConnectHandler(**remote_device)
     except NetMikoAuthenticationException:
-        print(f"Failed to execute CLI on {switch['host']} due to incorrect credentials.")
+        print(f"Failed to execute CLI on {remote_device['host']} due to incorrect credentials.")
         return
     except (NetMikoTimeoutException, SSHException):
-        print(f"Failed to execute CLI on {switch['host']} due to timeout or SSH not enabled.")
+        print(f"Failed to execute CLI on {remote_device['host']} due to timeout or SSH not enabled.")
         return
     except ValueError:
-        print(f"Unsupported platform {switch['host']}, {switch['device_type']}.")
+        print(f"Unsupported platform {remote_device['host']}, {remote_device['device_type']}.")
         return
     else:
         # IOS, IOS XE & NX-OS
@@ -92,9 +92,9 @@ def find_mac_address(switch, mac_address, results_list):
                 int_description = re.search(r"Description: (.+)", cli_output2)
                 int_description = int_description.group(1).rstrip() if int_description else ""
                 if best_match == "cisco_nxos":
-                    results_list.append([switch["host"], cli_items[-1], int_description, cli_items[1]])
+                    results_list.append([remote_device["host"], cli_items[-1], int_description, cli_items[1]])
                 else:
-                    results_list.append([switch["host"], cli_items[-1], int_description, cli_items[0]])
+                    results_list.append([remote_device["host"], cli_items[-1], int_description, cli_items[0]])
             device.disconnect()
 
         # JunOS
@@ -128,8 +128,30 @@ def find_mac_address(switch, mac_address, results_list):
                     int_description = int_description.group(1).rstrip() if int_description else ""
                 else:
                     continue
-                results_list.append([switch["host"], cli_items[-1], int_description, cli_items[0]])
+                results_list.append([remote_device["host"], cli_items[-1], int_description, cli_items[0]])
             device.disconnect()
+
+        # Arista EOS
+        elif best_match == "arista_eos":
+            # Grab MAC address table & extract information
+            cli_output = device.send_command(f"show mac address-table | include {mac_address}")
+            if cli_output is None or len(cli_output) == 0:
+                device.disconnect()
+                return
+            cli_output = cli_output.split("\n")
+            # Iterate through results
+            for cli_line in cli_output:
+                cli_items = cli_line.split()
+                # Skip empty result lines
+                if not cli_items:
+                    continue
+                cli_output2 = device.send_command(f"show interfaces {cli_items[3]}")
+                int_description = re.search(r"Description: (.+)", cli_output2)
+                int_description = int_description.group(1).rstrip() if int_description else ""
+                results_list.append([remote_device["host"], cli_items[3], int_description, cli_items[0]])
+            device.disconnect()
+
+        # Unsupported, disconnect
         else:
             device.disconnect()
 
